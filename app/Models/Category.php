8a32087e5d4f531baba33e598bd71dbe6c0357bd
ddaniel987 <unwill35@gmail.com>
2023-07-04 14:19:02 +0300

    Categories, Filters & translated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,16 @@ class Category extends Model
 {
     use HasFactory;
 
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
     public static function all($columns = ['*'])
     {
         $categories = parent::all($columns);
--- database/migrations/2023_07_03_054920_categories.php
+++ database/migrations/2023_07_03_054920_categories.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->text("name");
+            $table->unsignedBigInteger("parent_id");
             $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
