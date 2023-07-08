4ba06fa5274167135683d69961de46a7ef25c582
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-07-08 16:05:16 +0300

    filters testing

--- resources/js/components/hooks/useOpenState.jsx
+++ resources/js/components/hooks/useOpenState.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function useOpenState(initialState = false) {
   const [open, setOpen] = useState(initialState);
-
+  const toggleOpen = () => setOpen(!open);
+  
   useEffect(() => {
     document.body.style.overflow = open ? "hidden" : "auto";
 
@@ -10,8 +11,6 @@ export default function useOpenState(initialState = false) {
       document.body.style.overflow = "auto";
     }
   }, [open]);
-
-  const toggleOpen = () => setOpen(!open);
 
   return { open, setOpen, toggleOpen };
 }
